2ebd92e7926ae97f17298f3e5ded337046740e5f
hamza <mahjoubhamza036@gmail.com>
2023-07-25 17:22:20 +0200

    removed commented code

--- backend/src/configs/morgan.ts
+++ backend/src/configs/morgan.ts
@@ -3,8 +3,6 @@ import config from './config';
 import logger from './logger';
 import { Handler } from 'express';
 
-// morgan.token('message', (req, res) => res.locals.errorMessage || '');
-
 const getIpFormat = () => (config.env === 'production' ? ':remote-addr - ' : '');
 const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`;
 const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`;
